b298a57368411b29233682e18b8faeae98d11b31
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-01 12:38:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 // Your code here
+// Function to create an employee record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName: firstName,
+      familyName: familyName,
+      title: title,
+      payPerHour: payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function to create multiple employee records from an array
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+  }
+  
+  // Function to create a time-in event
+  function createTimeInEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return employee;
+  }
+  
+  // Function to create a time-out event
+  function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return employee;
+  }
+  
+  // Function to calculate hours worked on a specific date
+  function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(event => event.date === date);
+    let timeOut = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100; // Convert to hours
+  }
+  
+  // Function to calculate wages earned on a specific date
+  function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+  }
+  
+  // Function to calculate total wages for an employee
+  function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, event) => {
+      return total + wagesEarnedOnDate(employee, event.date);
+    }, 0);
+  }
+  
+  // Function to calculate payroll for all employees
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => {
+      return total + allWagesFor(employee);
+    }, 0);
+  }
+  
+  // Export functions for testing (Node.js)
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll
+  };
+  
